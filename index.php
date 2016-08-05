8399d14010cdaacf958fc9ac83a9669d0669ec16
Konstantin Bondarenko <89511582818@ya.ru>
2016-08-05 11:00:03 +0300

    my first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="windows-1251">
+	<title>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, unde.</title>
+</head>
+<body>
+<header>
+<div class="container-fluid">
+<div class="row">
+	<div class="col-lg-3 col-md-4 col-xs-12 header-columns">1. Lorem ipsum dolor sit amet.<br>1. Lorem ipsum dolor sit amet.</div>
+	<div class="col-lg-3 col-md-4 col-xs-6 header-columns">2. Lorem ipsum.</div>
+	<div class="col-lg-3 col-md-4 col-xs-6 header-columns">3. Lorem ipsum dolor sit amet, consectetur.</div>
+	<div class="col-lg-3 hidden-md col-xs-12 header-columns"><div class="my-button">PUSH IT</div></div>
+</div>
+</div>
+</header>
+
+<main>
+<div class="container">
+<div class="row">
+	<div class="col-md-3 main-columns">1. Lorem ipsum dolor sit amet.<br>1. Lorem ipsum dolor sit amet.</div>
+	<div class="col-md-3 main-columns">2. Lorem ipsum dolor sit amet.2</div>
+	<div class="col-md-3 main-columns">3. Lorem ipsum dolor sit amet.3</div>
+	<div class="col-md-3 main-columns">4. Lorem ipsum dolor sit amet.3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, inventore.</div>
+</div>
+</div>
+</main>
+</body>
+</html>
